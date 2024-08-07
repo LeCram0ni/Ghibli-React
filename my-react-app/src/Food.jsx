1bfb0bc19037b6e393486d6586aa754d703f91c1
marcel <15247414+LeCram0ni@users.noreply.github.com>
2024-08-07 18:08:37 +0200

    first

---
+++ my-react-app/src/Food.jsx
@@ -0,0 +1,11 @@
+function Food() {
+  return (
+    <ul>
+      <li>Apple</li>
+      <li>Banana</li>
+      <li>Strawberry</li>
+    </ul>
+  );
+}
+
+export default Food;
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,33 @@
+function Header() {
+  let name = null;
+  let text = "";
+  if (name != null) {
+    text = "waddup";
+  }
+
+  return (
+    <header>
+      <h1>My website {text}</h1>
+
+      <nav>
+        <ul>
+          <li>
+            <a href="https://www.google.de/?hl=de">Home</a>
+          </li>
+          <li>
+            <a href="https://www.google.de/?hl=de">About</a>
+          </li>
+          <li>
+            <a href="https://www.google.de/?hl=de">Services</a>
+          </li>
+          <li>
+            <a href="https://www.google.de/?hl=de">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <hr></hr>
+    </header>
+  );
+}
+
+export default Header;
